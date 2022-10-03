753e5762b7997419ef76add3f6e33a8c2b6d3fbb
ozgunsarioguz <ozgunsarioguz@live.com>
2022-10-03 12:50:27 +0300

    commit

---
+++ force-app/main/default/lwc/currencyWidget/currencyWidget.js
@@ -0,0 +1,41 @@
+import { LightningElement, track } from 'lwc';
+
+export default class WeatherPage extends LightningElement {
+
+    @track usdRate;
+    @track eurRate;
+
+    connectedCallback(){
+        this.listener();
+    }
+
+    listener(){
+        let long;
+        let lat;
+        console.log('hey');
+
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log(long);
+            console.log(lat);
+
+            const api = `https://v6.exchangerate-api.com/v6/latest/TRY`;
+            console.log(api);
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data=> {
+                console.log(data);
+                this.usdRate = (1 / (data.conversion_rates.USD)).toFixed(2);
+                console.log(this.usdRate);
+                this.eurRate = (1 / (data.conversion_rates.EUR)).toFixed(2);
+                console.log(this.eurRate);
+
+            })
+            });
+        }    
+    }
+}
